b636e6b07c5a611da63efb4fc12ba3e49c52709b
Mohamed Alaa Eldin <159617907+moha1999s@users.noreply.github.com>
2024-10-08 13:08:21 -0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+
+  darkMode: 'class',
+  content:['./src/**/*.{html,js}', 
+    
+    ],
+   
+    theme: {
+      extend: {
+        
+        fontFamily: {
+          Inter: ['Inter', 'sans-serif'],
+        },
+      },
+    },
+    plugins: [],
+  }
+
